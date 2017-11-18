4689fe5c58d5c2c7d64e04e98a26fe2e8ccb5573
Sean Davis <seandavi@gmail.com>
2017-11-18 12:26:46 -0500

    Site rebuild Sat Nov 18 12:26:46 EST 2017

---
+++ public/post/auto/november-2017-bibliography.el
@@ -0,0 +1,13 @@
+(TeX-add-style-hook
+ "november-2017-bibliography"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "Jurtz2017-pu"
+    "Hicks2017-mx"
+    "Huang2017-mr"
+    "Cui2017-fy"
+    "Spill2017-xz"
+    "Newton2017-rg"
+    "Hu2017-jo"))
+ :bibtex)
+
