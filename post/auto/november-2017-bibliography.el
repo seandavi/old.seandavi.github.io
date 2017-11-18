0e7a6b27855ed8cbcedaa083f5c0f5f63bf63004
Sean Davis <seandavi@gmail.com>
2017-11-18 13:11:05 -0500

    Site rebuild Sat Nov 18 13:11:05 EST 2017

--- post/auto/november-2017-bibliography.el
+++ post/auto/november-2017-bibliography.el
@@ -3,12 +3,14 @@
  (lambda ()
    (LaTeX-add-bibitems
     "Steinbaugh2017-mt"
+    "Barron2017-eh"
+    "Antaki2017-go"
+    "Sauria2017-vw"
     "Jurtz2017-pu"
     "Hicks2017-mx"
     "Huang2017-mr"
     "Cui2017-fy"
     "Spill2017-xz"
-    "Newton2017-rg"
     "Hu2017-jo"))
  :bibtex)
 
